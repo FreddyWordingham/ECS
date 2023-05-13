5259ab26d9c5ba931e48f9c8e4c74460d365c515
FreddyWordingham <freddy@digilab.co.uk>
2023-05-13 22:48:29 +0100

    Added Camera and Player startup spawning

--- src/bin/rendering.rs
+++ src/bin/rendering.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
 
 // == Main ==
 fn main() {
@@ -11,9 +11,44 @@ fn main() {
 // == Plugins ==
 struct GamePlugin;
 impl Plugin for GamePlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(spawn_camera)
+            .add_startup_system(spawn_player);
+    }
 }
 
 // == Components ==
 
+#[derive(Component)]
+struct Player {}
+
 // == Systems ==
+fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let windows = window_query.get_single().unwrap();
+    let width = windows.width();
+    let height = windows.height();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(width * 0.5, height * 0.5, 0.0),
+        ..default()
+    });
+}
+
+fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+    let width = window.width();
+    let height = window.height();
+
+    commands.spawn((
+        Player {},
+        SpriteBundle {
+            transform: Transform::from_xyz(width / 2.0, height / 2.0, 0.0),
+            texture: asset_server.load("sprites/ball_blue_large.png"),
+            ..default()
+        },
+    ));
+}
